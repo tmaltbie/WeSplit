a8f7ccf739e3cf8409972b4714136aa44964df7a
trevor <trevor@M1.local>
2021-03-18 23:16:01 -0700

    first commit

---
+++ WeSplit/ContentView.swift
@@ -0,0 +1,35 @@
+//
+//  ContentView.swift
+//  WeSplit
+//
+//  Created by trevor on 3/18/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    @State private var checkAmount = ""
+    @State private var numberOfPeople = 2
+    @State private var tipPercentage = 2
+    
+    let tipPercentages = [10, 15, 20, 25, 0]
+    
+    var body: some View {
+        Form {
+            Section {
+                TextField("Amount", text: $checkAmount)
+                    .keyboardType(.decimalPad)
+            }
+            
+            Section {
+                Text("$\(checkAmount)")
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by trevor on 3/18/21.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
